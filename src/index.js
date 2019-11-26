3276e9a6848b36549dfffefe9cc221ee303655a8
Eric Kim <hyeokjungkim@gmail.com>
2019-11-26 10:43:21 -0500

    delete function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 let quoteListUL = document.querySelector("#quote-list")
+let form = document.querySelector("#new-quote-form")
 
 fetch("http://localhost:3000/quotes?_embed=likes")
   .then(r => r.json())
@@ -14,23 +15,79 @@ function turnObjToHTML(quoteObj){
   let quoteLi = document.createElement("li")
     quoteLi.className = "quote-card"
 
-  quoteLi.innerHTML = `<blockquote class="blockquote">
-      <p class="mb-0">${quoteObj.quote}</p>
-      <footer class="blockquote-footer">${quoteObj.author}</footer>
-      <br>
-      <button class='btn-success'>Likes: <span>${quoteObj.likes.length}</span></button>
-      <button class='btn-danger'>Delete</button>
-    </blockquote>
-  `
+  let blockquote = document.createElement("blockquote")
+    blockquote.className = "blockquote"
 
-  quoteListUL.append(quoteLi);
-//   <li class='quote-card'>
-//   <blockquote class="blockquote">
-//     <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-//     <footer class="blockquote-footer">Someone famous</footer>
-//     <br>
-//     <button class='btn-success'>Likes: <span>0</span></button>
-//     <button class='btn-danger'>Delete</button>
-//   </blockquote>
-// </li>
+  let quoteP = document.createElement("p")
+    quoteP.innerText = quoteObj.quote
+    quoteP.className = "mb-0"
+
+  let authorFooter = document.createElement("footer")
+    authorFooter.innerText = quoteObj.author
+    authorFooter.className = "blockquote-footer"
+
+  let lineBreak = document.createElement("br")
+
+  let likeButton = document.createElement("button")
+    likeButton.className = "btn-success"
+
+  let likesSpan = document.createElement("span")
+    likesSpan.innerText = quoteObj.likes.length
+
+  let deleteButton = document.createElement("button")
+    deleteButton.className = "btn-danger"
+    deleteButton.innerText = "Delete"
+
+
+
+
+  quoteLi.append(blockquote)
+  blockquote.append(quoteP, authorFooter, lineBreak, likeButton, deleteButton)
+  // likeButton.append("Likes: ", likesSpan)
+  likeButton.innerText = "Likes: "
+  likeButton.append(likesSpan)
+
+  quoteListUL.append(quoteLi)
+
+  quoteListUL.append(quoteLi)
+
+
+
+  likeButton.addEventListener("click", () => {
+    console.log(quoteObj.id);
+  })
+
+  deleteButton.addEventListener("click", () => {
+
+    fetch(`http://localhost:3000/quotes/${quoteObj.id}`, {
+      method: "DELETE"
+    })
+    .then(r => r.json())
+    .then((blankObj) => {
+      quoteLi.remove()
+    })    
+
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
+
+
+
+form.addEventListener("submit", (evt) => {
+  evt.preventDefault()
+})
